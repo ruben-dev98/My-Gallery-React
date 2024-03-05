20da71722a6f2652bfb3c0b1775134ed402fef9f
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-03-05 21:02:22 +0100

    FIX: delete App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { FavoritesPage } from './pages/FavoritesPage';
+import { MainPage } from './pages/MainPage';
 import { store } from './app/store'
-import App from './app/App'
 import './index.css'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<MainPage />}>
+    <Route index element={<HomePage />} />
+    <Route path='/search' element={<HomePage />} />
+    <Route path='/favorites' element={<FavoritesPage />} />
+  </Route>
+));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App></App>
-  </Provider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
